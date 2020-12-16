f102246ecc1797548a013c4c6cda45a1e8c00197
elixz196123 <71353800+elixz196123@users.noreply.github.com>
2020-12-15 21:56:14 -0800

    Add files via upload

---
+++ nuevealcuarentaytres.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //imprimir los enteros del 9 al el 43
+    for(int i = 9; i <44; i++)
+    {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
